cd50c8fcd8a8118d5bc6b48a33ab2d56b337a4cc
Eduardo Oliveira <edumottaoliveira@gmail.com>
2024-07-01 17:38:03 -0700

    Improve home layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import HomeScreen from "./screens/HomeScreen";
-import { Text, View, Button, SafeAreaViewComponent } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { Stack } from 'expo-router';
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,17 +7,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <HomeScreen />
     </SafeAreaView>
-      
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: "auto",
+    width: "100%",
+    maxWidth: 800
+  },
+});
--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -17,9 +17,12 @@ enum CardCategory {
 }
 
 const items = [
-  { id: '1', title: 'Item 1', category: CardCategory.Person },
-  { id: '2', title: 'Item 2', category: CardCategory.Place },
-  { id: '3', title: 'Item 3', category: CardCategory.Thing },
+  { id: '1', title: 'Mickey Mouse', category: CardCategory.Person },
+  { id: '2', title: 'Sacramento', category: CardCategory.Place },
+  { id: '3', title: 'Baseball Bat', category: CardCategory.Thing },
+  { id: '4', title: 'Shakira', category: CardCategory.Person },
+  { id: '5', title: 'Mars', category: CardCategory.Place },
+  { id: '6', title: 'Eraser', category: CardCategory.Thing },
   // Add more items as needed
 ];
 
@@ -42,25 +45,75 @@ const HomeScreen: React.FC = () => {
     router.push('screens/CreateScreen')
   };
 
-  function CategoryIcon (category: CardCategory): React.JSX.Element {
+  function CategoryIcon(category: CardCategory): React.JSX.Element {
     switch (category) {
-      case CardCategory.Person: 
-        return <FontAwesome6 name='face-grin-wide'  size={24} color='black' />
-      case CardCategory.Place: 
-        return <FontAwesome6 name='location-dot'  size={24} color='black' />
-      case CardCategory.Thing: 
-        return <FontAwesome6 name='cube'  size={24} color='black' />
+      case CardCategory.Person:
+        return <FontAwesome6 name='face-grin-wide' size={18} color='black' />
+      case CardCategory.Place:
+        return <FontAwesome6 name='location-dot' size={18} color='black' />
+      case CardCategory.Thing:
+        return <FontAwesome6 name='cube' size={18} color='black' />
     }
   }
 
+  const styles = StyleSheet.create({
+    container: {
+      // flex: 1,
+    },
+    navBar: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingTop: 50,
+      paddingHorizontal: 10,
+      paddingBottom: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ccc',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+    createButton: {
+      paddingHorizontal: 16,
+    },
+    createButtonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    searchBar: {
+      height: 40,
+      borderColor: '#bbb',
+      borderWidth: 1,
+      borderRadius: 6,
+      margin: 10,
+      paddingHorizontal: 10,
+      color: search != '' ? '#000' : '#aaa',
+    },
+    itemContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 10,
+      margin: 8,
+      borderColor: "#eee",
+      backgroundColor: '#fff',
+      borderWidth: 1,
+      borderRadius: 8
+    },
+    itemTitle: {
+      fontSize: 16,
+    },
+    iconText: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 8
+    }
+  });
+
   return (
     <View style={styles.container}>
-      {/* <View style={styles.navBar}>
-        <Text style={styles.title}>Home</Text>
-        <TouchableOpacity onPress={handleCreatePress} style={styles.createButton}>
-          <Text style={styles.createButtonText}>Create</Text>
-        </TouchableOpacity>
-      </View> */}
       <Stack.Screen
         options={{
           title: 'Played Cards',
@@ -76,85 +129,26 @@ const HomeScreen: React.FC = () => {
         onChangeText={setSearch}
       />
       <FlatList
-
         data={filteredItems}
         keyExtractor={item => item.id}
+        scrollEnabled={true}
         renderItem={({ item }) => (
           <TouchableOpacity
             style={styles.itemContainer}
             onPress={() => handleItemPress(item.id)}
           >
             <View style={styles.iconText}>
-              { CategoryIcon(item.category) }
+              {CategoryIcon(item.category)}
               <Text style={styles.itemTitle}>{item.title}</Text>
             </View>
             <Text>10 hints revealed</Text>
-            
+
           </TouchableOpacity>
         )}
       />
+
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  navBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: 50,
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  createButton: {
-    // backgroundColor: '#007bff',
-    paddingHorizontal: 16,
-    // borderRadius: 5,
-  },
-  createButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  searchBar: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    margin: 10,
-    paddingHorizontal: 10,
-  },
-  itemContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 12,
-    margin: 8,
-    // borderBottomWidth: 1,
-    // borderBottomColor: '#ccc',
-    backgroundColor: '#ccc',
-    borderColor: "#bbb",
-    borderWidth: 1,
-    borderRadius: 8
-  },
-  itemTitle: {
-    fontSize: 18,
-  },
-  iconText: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8
-  }
-});
 
 export default HomeScreen
