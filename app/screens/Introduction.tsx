bb9fb32873492a6f2ef576f80c6d8ac9a67e14b6
Eduardo Oliveira <edumottaoliveira@gmail.com>
2025-02-22 18:15:50 -0800

    Add animations in Introduction screen

--- app/screens/Introduction.tsx
+++ app/screens/Introduction.tsx
@@ -1,39 +1,71 @@
-import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Dimensions, Platform } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, Dimensions, Platform, Animated, useAnimatedValue, Easing } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import PrimaryButton from '../uiComponents/PrimaryButton';
 import { MysteryIcon, FileIcon } from '../uiComponents/Icons';
 import { Color } from '../uiComponents/Colors';
-import Confetti from '../uiComponents/ConfettiAnimation';
 
 const { width, height } = Dimensions.get('window');
 
 const IntroductionScreen: React.FC = () => {
   const router = useRouter()
+  const opacityIlustration = useRef(new Animated.Value(0)).current;
+  const xPositionIcons = useRef(new Animated.Value(0)).current;
+  // const xPositionIcons = useAnimatedValue(0)
 
   const handlePlayPress = () => {
     router.push('screens/Setup')
   };
 
+  useEffect(() => {
+    Animated.loop(
+      // Animated.sequence([
+        Animated.timing(xPositionIcons, {
+          toValue: width,
+          duration: 10000,
+          useNativeDriver: true,
+          easing: Easing.linear
+        }),
+      // ])
+    ).start();
+
+    Animated.stagger(300, [
+      Animated.timing(opacityIlustration, {
+        toValue: 1,
+        duration: 800,
+        useNativeDriver: true,
+        easing: Easing.exp
+      }),
+    ]).start();
+  }, [xPositionIcons, opacityIlustration]);
+
+  const FileIconRow: React.FC = () => (
+    <Animated.View style={[styles.iconContainer, {transform: [{translateX: xPositionIcons}]}]}>
+      {
+        [...Array(9)].map((_, index) => (
+          <FileIcon key={index} size={height * 0.05}/>
+        ))
+      }
+    </Animated.View>
+  );
+    
   return (
     <SafeAreaView style={styles.container}>
       <LinearGradient
         colors={['#2c3e50', '#34495e', '#2c3e50']}
         style={styles.gradient}
       >
-        <Confetti />
         <View style={styles.content}>
-          <MysteryIcon size={height * 0.15}/>
+          <Animated.View style={{opacity: opacityIlustration}}>
+            <MysteryIcon size={height * 0.15}/>
+          </Animated.View>
+          
           <Text style={styles.title}>20 Hints</Text>
           <Text style={styles.description}>
             Uncover secret words. Decipher clues. Become the ultimate word detective.
           </Text>
-          <View style={styles.iconContainer}>
-            <FileIcon size={height * 0.05}/>
-            <FileIcon size={height * 0.05}/>
-            <FileIcon size={height * 0.05}/>
-          </View>
+          <FileIconRow />
           <PrimaryButton title={'Investigate'} onPress={() => handlePlayPress()} />
         </View>
       </LinearGradient>
@@ -84,9 +116,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: width * 0.05,
   },
   iconContainer: {
+    width: 3*width,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    gap: width / 6
+    // gap: width / 6,
+    alignItems: 'center',
+    // backgroundColor: '#FBA',
   }
 });
 
