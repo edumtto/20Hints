b4f3f567177aa008c26b50f3060759ad5445b1fc
Eduardo Oliveira <edumottaoliveira@gmail.com>
2025-05-16 18:23:31 -0700

    Tint iOS status bar white

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,31 @@
+import React from 'react';
 import { Stack } from "expo-router";
+import { StatusBar, Platform } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack 
-      screenOptions={{
-        headerShown: false,
-        headerStyle: {
-          backgroundColor: 'rgb(255 240 0)',
-        },
-        // headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontSize: 24,
-          fontWeight: 'bold'
-        }
-      }}
-    >
-      <Stack.Screen
-        name="screens/game/Play"
-        options={{ headerShown: false }}
-      />
-    </Stack>
+    <>
+      {Platform.OS === 'ios' && (
+        <StatusBar barStyle="light-content" />
+      )}
+      <Stack 
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {
+            backgroundColor: 'rgb(255 240 0)',
+          },
+          // headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontSize: 24,
+            fontWeight: 'bold'
+          }
+        }}
+      >
+        <Stack.Screen
+          name="screens/game/Play"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
   );
 }
